f8da4b7a750b2f920e9b0a7068cdb7ef96e1e856
Arseniy Maslov <mypkaka@gmail.com>
2026-09-07 15:36:17 +0300

    edit pep8

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,9 +1,6 @@
-# backend/api/tests.py
-from http import HTTPStatus
-
-from django.test import Client, TestCase
-
 from api import models
+from django.test import Client, TestCase
+from http import HTTPStatus
 
 
 class TaskiAPITestCase(TestCase):
