d720c4e77a3acbbd99664526d621132ce58e1548
Arseniy Maslov <mypkaka@gmail.com>
2026-09-07 19:54:26 +0300

    Добваил недостающую часть

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_URL = BASE_DIR / 'collected_static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
