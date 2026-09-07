cfbb62968f406b1ec2ce1d9373fdbdeed6545fed
Arseniy Maslov <mypkaka@gmail.com>
2026-09-07 20:32:25 +0300

    fix atribute

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_URL = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
